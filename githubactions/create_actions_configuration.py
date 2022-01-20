b27f64b09af732d831bdcb60f3f057dbf2bf4e59
colemank <colemank@allegheny.edu>
2022-01-20 10:32:18 -0500

    Complete generation of GitHub Actions configuration

--- githubactions/create_actions_configuration.py
+++ githubactions/create_actions_configuration.py
@@ -1,11 +1,4 @@
 import yaml
 
-build_file = open('.github/workflows/gatorgradle.yml', 'w')
-
-yaml.dump({'name': 'GatorGradle', 'on': ['push', 'pull_request'], 'jobs':
-    {'grade': {'runs-on': 'ubuntu-latest', 'steps': [
-        {'name': 'Checkout repository', 'uses': 'actions/checkout@v2'},
-        {'name': 'Setup Python 3.9', 'uses': 'actions/setup-python@v2', 'id': 'setup-python', 'with': {'python-version': '3.9'}},
-        {'name': 'Check for existence of gradle files', 'uses': 'andstor/file-existence-action@v1', 'with': {'files': 'build.gradle, gradle.properties, settings.gradle, gatorgrader.yml'}},
-        {'name': 'Determine gradle version', 'run': 'gradle --version'},
-        {'name': 'Run GatorGrader', 'run': 'gradle grade'}]}}}, build_file)
+build_file = open('../.github/workflows/grade.yml', 'w')
+yaml.dump({'name': 'Grade', True: ['push'], 'jobs': {'grade': {'runs-on': 'ubuntu-latest', 'steps': [{'name': 'Checkout repository', 'uses': 'actions/checkout@v2'}, {'name': 'Run GatorGradle', 'uses': 'GatorEducator/gatorgradle-action@v1'}]}}}, build_file)
