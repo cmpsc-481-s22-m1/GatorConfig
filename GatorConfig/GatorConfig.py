4aeb3b8dd03ae821954d964ef5e7c02c3442ba03
Wesley Long <46755903+WesleyL30@users.noreply.github.com>
2022-01-20 10:31:02 -0500

    Simplify CLI parameter name

--- GatorConfig/GatorConfig.py
+++ GatorConfig/GatorConfig.py
@@ -4,15 +4,15 @@ from typing import List
 cli = typer.Typer()
 
 @cli.command()
-def main(
+def cli_input(
     name: str = typer.Option("Project"),
     brk: bool = typer.Option(False, "--break"),
     fastfail: bool = typer.Option(False),
-    files: List[str] = typer.Option([]),
+    file: List[str] = typer.Option([]),
     indent: int = typer.Option(4),
     commit_count: int = typer.Option(5)
 ):
-    typer.echo(f"Name: {name}\nBreak: {brk}\nFastfail: {fastfail}\nFiles: {files}\nIndent: {indent}\nCommit Count: {commit_count}\n")
+    print(f"Name: {name}\nBreak: {brk}\nFastfail: {fastfail}\nFiles: {file}\nIndent: {indent}\nCommit Count: {commit_count}\n")
 
 if __name__ == "__main__":
     cli()
