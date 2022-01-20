be18a29ed0bbe9ab8cbc2c81690adf75d023e9b4
Wesley Long <46755903+WesleyL30@users.noreply.github.com>
2022-01-20 10:08:57 -0500

    Replace placeholder values

--- GatorConfig/GatorConfig.py
+++ GatorConfig/GatorConfig.py
@@ -1,13 +1,18 @@
 import typer
+from typing import List
 
 cli = typer.Typer()
 
 @cli.command()
 def main(
-    placeholder1: bool = typer.Option(False),
-    placeholder2: int = typer.Option(10)
+    name: str = typer.Option("Project"),
+    brk: bool = typer.Option(False, "--break"),
+    fastfail: bool = typer.Option(False),
+    files: List[str] = typer.Option([]),
+    indent: int = typer.Option(4),
+    commit_count: int = typer.Option(5)
 ):
-    typer.echo(f"Placeholder 1: {placeholder1}\nPlaceholder 2: {placeholder2}")
+    typer.echo(f"Name: {name}\nBreak: {brk}\nFastfail: {fastfail}\nFiles: {files}\nIndent: {indent}\nCommit Count: {commit_count}\n")
 
 if __name__ == "__main__":
     cli()
