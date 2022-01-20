d39c3a5e73905570866045d7f611a0bb74b0c84f
colemank <colemank@allegheny.edu>
2022-01-20 12:17:50 -0500

    Fix generation of configuration file

--- githubactions/create_actions_configuration.py
+++ githubactions/create_actions_configuration.py
@@ -1,4 +1,19 @@
-import yaml
+import ruamel.yaml as yaml
 
 build_file = open('../.github/workflows/grade.yml', 'w')
-yaml.dump({'name': 'Grade', True: ['push'], 'jobs': {'grade': {'runs-on': 'ubuntu-latest', 'steps': [{'name': 'Checkout repository', 'uses': 'actions/checkout@v2'}, {'name': 'Run GatorGradle', 'uses': 'GatorEducator/gatorgradle-action@v1'}]}}}, build_file)
+
+yaml_str = """\
+name: Grade
+on: [push]
+jobs:
+  grade:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v2
+      - name: Run GatorGradle
+        uses: GatorEducator/gatorgradle-action@v1
+"""
+
+data = yaml.round_trip_load(yaml_str)
+yaml.round_trip_dump(data, build_file, indent=4)
