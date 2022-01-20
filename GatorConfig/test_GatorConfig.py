242bed673c0095713254af52cebc4b6fedd8d2ba
Wesley Long <46755903+WesleyL30@users.noreply.github.com>
2022-01-20 11:24:51 -0500

    Fix GatorConfig Test

--- GatorConfig/test_GatorConfig.py
+++ GatorConfig/test_GatorConfig.py
@@ -1,11 +1,17 @@
 # import pytest
 from typer.testing import CliRunner
-from GatorConfig import GatorConfig
 from .GatorConfig import cli
 
 runner = CliRunner()
 
 def test_cli_input():
-    result = runner.invoke(cli, "--name", "Test", "--break", "--fastfail")
+    result = runner.invoke(cli, ["--name", "Test", "--break", "--fastfail", "--file", "Object 1", "--file", "Object 2", "--indent", 6, "--commit-count", 6])
     assert result.exit_code == 0
+    assert "Name: Test" in result.stdout
+    assert "Break: True" in result.stdout
+    assert "Fastfail: True" in result.stdout
+    assert "Files: (\'Object 1\', \'Object 2\')" in result.stdout
+    assert "Indent: 6" in result.stdout
+    assert "Commit Count: 6" in result.stdout
+
     
