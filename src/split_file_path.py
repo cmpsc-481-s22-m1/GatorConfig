5224bb56484f39a6bc04eea44d02350dbe45a6b4
PaigeCD <downeyp@allegheny.edu>
2022-01-21 10:44:23 -0500

    address linting errors

--- src/split_file_path.py
+++ src/split_file_path.py
@@ -1,11 +1,11 @@
-# separates directories from files paths and stores them as dictionaries inside a dictionary
-def split_file_path(path_dic):
+"""Split file path module."""
+def split_file_path(paths):
+    """Separates directories from files paths and stores them as dictionaries inside a dictionary."""
     output = {}
-    for key, value in path_dic.items():
+    for key, value in paths.items():
         directories = key.split('/')
         dir_dic = output
         for direc in directories[:-1]:
-            # direc = direc + "/" # adds / to end of dir
             if direc not in dir_dic:
                 dir_dic[direc] = {}
             dir_dic = dir_dic[direc]
@@ -15,10 +15,9 @@ def split_file_path(path_dic):
 
 # example dictionary containing three file paths
 path_dic = {
-    'src/main/java/samplelab/SampleLabMain.java': [''], 
+    'src/main/java/samplelab/SampleLabMain.java': [''],
     'src/main/java/samplelab/DataClass.java': [''],
     'writing/reflection.md': ['']
 }
 final_dic = split_file_path(path_dic)
 print(final_dic)
-# {'src': {'main': {'java': {'samplelab': {'SampleLabMain.java': [''], 'DataClass.java': ['']}}}}, 'writing': {'reflection.md': ['']}}
