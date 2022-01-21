f2f79eaf95931c51a0fdb74a97d031f3f8e82cfb
Danny Ullrich <42869122+ullrichd21@users.noreply.github.com>
2022-01-21 18:47:04 -0500

    Fixing lint errors.

--- src/gator_yaml.py
+++ src/gator_yaml.py
@@ -2,7 +2,10 @@
 
 
 class GatorYaml:
+    """Main GatorYaml object"""
     def __init__(self, indent=4, spaces=4):
+        """Init GatorYAML object. Takes optional arguments to change indent of files
+        and how many spaces is considered a tab """
         self.spaces = spaces  # How many spaces is a tab
         self.tabs = -1  # Current tab level
         self.output = ""  # Init output
@@ -16,6 +19,7 @@ class GatorYaml:
         return self.output
 
     def enum_list(self, list_in):
+        """Enumerate through input list and output each item unless it finds another dictionary."""
         for i in list_in:
             if isinstance(i, dict):
                 self.tabs += 1
@@ -23,33 +27,34 @@ class GatorYaml:
             else:
                 self.output_list_item(i)
 
-    def enum_dict(self, d):
+    def enum_dict(self, dic):
+        """Enumerate through input dictionary and output each key"""
         self.tabs += 1
 
-        for k in d.keys():
+        for k in dic.keys():
             if k == "indent":
-                self.indents = int(d[k])
+                self.indents = int(dic[k])
 
             if k == "files":
                 self.tabs -= 1
                 # if isinstance(d[k], list):
                 #     self.enum_list(d[k])
-                if isinstance(d[k], dict):
-                    self.enum_file_dict(d[k])
-                    pass
-            elif isinstance(d[k], list):
+                if isinstance(dic[k], dict):
+                    self.enum_file_dict(dic[k])
+            elif isinstance(dic[k], list):
                 self.output_key(k)
-                self.enum_list(d[k])
-            elif isinstance(d[k], dict):
+                self.enum_list(dic[k])
+            elif isinstance(dic[k], dict):
                 self.output_key(k)
-                self.enum_dict(d[k])
+                self.enum_dict(dic[k])
             else:
-                if not self.is_keyword(k, d[k]):
-                    self.output_key_value(k, d[k])
+                if not self.is_keyword(k, dic[k]):
+                    self.output_key_value(k, dic[k])
 
         self.tabs -= 1
 
     def enum_file_dict(self, files):
+        """Enumerate through the file list dictionary and output each key"""
         self.tabs += 1
 
         for k in files:
@@ -63,19 +68,24 @@ class GatorYaml:
         self.tabs -= 1
 
     def enum_file_list(self, list_in):
+        """Enumerate through each file key's parameter list items"""
         for item in list_in:
             self.output += (" " * self.spaces) * self.indents + str(item) + "\n"
 
     def output_list_item(self, item):
+        """Output a generic list item"""
         self.output += (" " * self.spaces) * self.tabs + " -" + str(item) + "\n"
 
     def output_key(self, key):
+        """Output a generic key"""
         self.output += (" " * self.spaces) * self.tabs + str(key) + ":\n"
 
     def output_key_value(self, key, value):
+        """Output a generic key and it's value"""
         self.output += ((" " * self.spaces) * self.tabs) + str(key) + ": " + str(value) + "\n"
 
     def is_keyword(self, key, value):
+        """Output key and value if a keyword"""
         if key in self.keywords:
             self.output += (" " * self.spaces) * self.tabs + str(key) + " " + str(value) + "\n"
             return True
