7ae0f98d4160f210d226afa05a9d013befa956fd
colemank <colemank@allegheny.edu>
2022-01-24 10:36:08 -0500

    Fix testing

--- tests/test_create_actions_configuration.py
+++ tests/test_create_actions_configuration.py
@@ -1,6 +1,6 @@
 """This module tests the githubactions.create_actions_configuration module"""
-import githubactions.create_actions_configuration
+import src.create_actions_configuration
 
 def test_create_configuration_file():
     """Check that the configuration file is actually created"""
-    githubactions.create_actions_configuration.create_configuration_file('config.yml')
+    src.create_actions_configuration.create_configuration_file('config.yml')
