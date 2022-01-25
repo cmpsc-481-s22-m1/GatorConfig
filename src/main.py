ce2ff66c36cf4902992916fa68f75f334548847f
colemank <colemank@allegheny.edu>
2022-01-25 10:12:14 -0500

    Implement actions configuration

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
+import actions_configuration
 import gator_config
 
 def main():
     gator_config.cli()
+    actions_configuration.create_configuration_file('../.github/workflows/grade.yml')
 
 if __name__ == "__main__":
     main()
