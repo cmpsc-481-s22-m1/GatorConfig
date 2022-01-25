ccae6a1dd1b45060091c5b9f0ddbb43584dd54fe
colemank <colemank@allegheny.edu>
2022-01-25 10:19:48 -0500

    Fix linting

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-import actions_configuration
-import gator_config
+"""This module generates the configuration files needed to automate GatorGradle"""
+from src import actions_configuration
+from src import gator_config
 
 def main():
+    """Generate the configuration files based on command line input"""
     gator_config.cli()
     actions_configuration.create_configuration_file('../.github/workflows/grade.yml')
 
