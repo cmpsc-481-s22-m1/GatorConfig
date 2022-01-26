fe630e08d0057f67177ad169748618c35ce3d9d3
Wesley Long <46755903+WesleyL30@users.noreply.github.com>
2022-01-26 10:00:14 -0500

    Add parsing of input

--- src/scrape_releases.py
+++ src/scrape_releases.py
@@ -3,7 +3,12 @@ from typing import List
 
 def get_github_releases(url: str) -> List:
     output = []
-    r = requests.get("https://api.github.com/repos/gatoreducator/gatorgrader/releases").json()
+    r = ""
+    if "https://api.github.com/repos/"  and "/releases" in url:
+        r = requests.get(url).json()
+    else:
+        repo = url.replace("https://github.com/", "")
+        r = requests.get(f"https://api.github.com/{repo}/releases")
     for item in r:
         output.append(item["name"])
     return output
