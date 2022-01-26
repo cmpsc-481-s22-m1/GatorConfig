961cc952912eb39365910534b055e2bd55702e1e
Danny Ullrich <42869122+ullrichd21@users.noreply.github.com>
2022-01-26 12:57:29 -0500

    Added requests-mock to test api calls. Fixed linting errors.

--- src/scrape_releases.py
+++ src/scrape_releases.py
@@ -1,14 +1,33 @@
-import requests
+"""Gets list of releases from GitHub"""
+import re
 from typing import List
+import requests
+
+
 
 def get_github_releases(url: str) -> List:
+    """Takes a URL or organization and repo and returns a list of all releases."""
     output = []
-    r = ""
-    if "https://api.github.com/repos/"  and "/releases" in url:
-        r = requests.get(url).json()
-    else:
+
+    if "/releases" in url:
+        response = requests.get(url).json()
+    elif "https://github.com/" in url:
         repo = url.replace("https://github.com/", "")
-        r = requests.get(f"https://api.github.com/{repo}/releases")
-    for item in r:
+        response = requests.get(f"https://api.github.com/repos/{repo}/releases").json()
+    else:
+        pat = re.compile("https://api.github.com/repos/(.*)/releases")
+        match = pat.search(url)
+
+        if match is None:
+            data = url.split("/")
+
+            if len(data) != 2:
+                raise Exception("Could not parse " + url)
+        else:
+            data = match.group(1).split("/")
+
+        response = requests.get(f"https://api.github.com/repos/{data[0]}/{data[1]}/releases").json()
+
+    for item in response:
         output.append(item["name"])
     return output
