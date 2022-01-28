cc0f826ad5864e1f688fce202b50537aba6f5909
Wesley Long <46755903+WesleyL30@users.noreply.github.com>
2022-01-27 19:23:46 -0500

    Add test case

--- tests/test_gator_config.py
+++ tests/test_gator_config.py
@@ -1,13 +1,13 @@
 """Test functions within gator_config"""
-import pathlib
 import pytest
 from typer.testing import CliRunner
+from src.gator_config import output_file
 from src.gator_config import cli
 
 runner = CliRunner()
 # @pytest.mark.skip(reason="no way of currently testing this")
 @pytest.fixture()
-def test_cli_input(tmpdir, mocker):
+def test_cli_input(mocker, tmpdir):
     """Test cli input with every flag"""
     test_dir = tmpdir.mkdir("testing")
     # pylint: disable=W0612
@@ -32,15 +32,12 @@ def test_cli_input(tmpdir, mocker):
         assert test_file.exists()
     with open(test_file, encoding='utf-8') as fle:
         #print(fle.read())
-        assert "name: Project" in fle.read()
+        assert "name: Test" in fle.read()
 
-#@pytest.mark.parametrize("expected",[""])
 @pytest.fixture()
-# pylint: disable=W0613
 def test_cli_no_input(mocker, tmpdir):
     """Test cli input with no flags"""
     test_dir = tmpdir.mkdir("testing")
-    # pylint: disable=W0612
     result = runner.invoke(cli)
     assert "Wrote file to: " in result.stdout()
     test_file = test_dir.joinpath("gatorgrader.yml")
@@ -51,7 +48,13 @@ def test_cli_no_input(mocker, tmpdir):
         assert "name: Project" in fle.read()
 
 
-# pytest.mark.parametrize()
+@pytest.mark.parametrize("input_text", ["I am text"])
 
-# def test_output_file():
-#     pass
+@pytest.fixture()
+def test_output_file(tmpdir, input_text):
+    test_dir = tmpdir.mkdir("testing")
+    test_file_path = test_dir.joinpath("gatorgrader.yml")    
+    assert test_file_path.exists() == False
+    output_file(input_text, test_dir)
+    assert test_file_path.exists()
+    assert "I am text" in test_file_path
