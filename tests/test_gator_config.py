962752d6a6e886af3d3d3dc65836d92caf73178c
Wesley Long <46755903+WesleyL30@users.noreply.github.com>
2022-01-27 22:10:09 -0500

    Rework test cases

--- tests/test_gator_config.py
+++ tests/test_gator_config.py
@@ -1,12 +1,12 @@
 """Test functions within gator_config"""
 import pytest
+from pytest_mock import MockerFixture
 from typer.testing import CliRunner
 from gatorconfig.gator_config import cli
 from gatorconfig.gator_config import output_file
 
 runner = CliRunner()
 # @pytest.mark.skip(reason="no way of currently testing this")
-@pytest.fixture()
 def test_cli_input(mocker, tmpdir):
     """Test cli input with every flag"""
     test_dir = tmpdir.mkdir("testing")
@@ -25,22 +25,19 @@ def test_cli_input(mocker, tmpdir):
         "--output-path",
         test_dir
         ])
-    assert "Wrote file to: " in result.stdout()
-    print("Test Output:", result.stdout())
-    test_file = test_dir.joinpath("gatorgrader.yml")
+    #print("Test Output:", result.stdout())
+    test_file = test_dir / "gatorgrader.yml"
     with mocker.patch('builtins.input', return_value=""):
         assert test_file.exists()
     with open(test_file, encoding='utf-8') as fle:
         #print(fle.read())
         assert "name: Test" in fle.read()
 
-@pytest.fixture()
-def test_cli_no_input(mocker, tmpdir):
+def test_cli_no_input(mocker: MockerFixture, tmpdir: MockerFixture):
     """Test cli input with no flags"""
     test_dir = tmpdir.mkdir("testing")
-    result = runner.invoke(cli)
-    assert "Wrote file to: " in result.stdout()
-    test_file = test_dir.joinpath("gatorgrader.yml")
+    result = runner.invoke(cli, ["--output-path", test_dir])
+    test_file = test_dir / "gatorgrader.yml"
     with mocker.patch('builtins.input', return_value=""):
         assert test_file.exists()
     with open(test_file, encoding='utf-8') as fle:
@@ -48,14 +45,15 @@ def test_cli_no_input(mocker, tmpdir):
         assert "name: Project" in fle.read()
 
 
-@pytest.mark.parametrize("input_text", ["I am text"])
+#@pytest.mark.parametrize("input_text", ["I am text"])
 
-@pytest.fixture()
-def test_output_file(tmpdir, input_text):
+def test_output_file(tmpdir):
     """Test creation of file, and if the file has the correct contents"""
+    input_text = "I am text"
     test_dir = tmpdir.mkdir("testing")
-    test_file_path = test_dir.joinpath("gatorgrader.yml")
+    test_file_path = test_dir / "gatorgrader.yml"
     assert test_file_path.exists() is False
     output_file(input_text, test_dir)
     assert test_file_path.exists()
-    assert "I am text" in test_file_path
+    with open(test_file_path) as test_file:
+        assert "I am text" in test_file.read()
