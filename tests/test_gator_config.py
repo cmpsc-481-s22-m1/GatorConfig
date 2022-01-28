5852e6ad66ac4fda2d4a9efbc55283ed18107629
Wesley Long <46755903+WesleyL30@users.noreply.github.com>
2022-01-27 19:04:56 -0500

    Add tmpdir in test cases

--- tests/test_gator_config.py
+++ tests/test_gator_config.py
@@ -1,15 +1,15 @@
 """Test functions within gator_config"""
 import pathlib
 import pytest
-import mock
 from typer.testing import CliRunner
 from src.gator_config import cli
 
 runner = CliRunner()
-
-@pytest.mark.skip(reason="no way of currently testing this")
-def test_cli_input():
+# @pytest.mark.skip(reason="no way of currently testing this")
+@pytest.fixture()
+def test_cli_input(tmpdir, mocker):
     """Test cli input with every flag"""
+    test_dir = tmpdir.mkdir("testing")
     # pylint: disable=W0612
     result = runner.invoke(cli, [
         "--name",
@@ -21,26 +21,33 @@ def test_cli_input():
         "--indent",
         6,
         "--commit-count",
-        6
+        6,
+        "--output-path",
+        test_dir
         ])
-    output = pathlib.Path.cwd().joinpath("gatorgrader.yml")
-    with mock.patch('builtins.input', return_value=""):
-        assert result.exit_code == 0
-        assert output.exists()
-    with open(output, encoding='utf-8') as fle:
+    assert "Wrote file to: " in result.stdout()
+    print("Test Output:", result.stdout())
+    test_file = test_dir.joinpath("gatorgrader.yml")
+    with mocker.patch('builtins.input', return_value=""):
+        assert test_file.exists()
+    with open(test_file, encoding='utf-8') as fle:
+        #print(fle.read())
         assert "name: Project" in fle.read()
 
-@pytest.mark.parametrize("expected",[""])
-
+#@pytest.mark.parametrize("expected",[""])
+@pytest.fixture()
 # pylint: disable=W0613
-def test_cli_no_input(expected):
+def test_cli_no_input(mocker, tmpdir):
     """Test cli input with no flags"""
+    test_dir = tmpdir.mkdir("testing")
     # pylint: disable=W0612
     result = runner.invoke(cli)
-    output = pathlib.Path.cwd().joinpath("gatorgrader.yml")
-    with mock.patch('builtins.input', return_value=""):
-        assert output.exists()
-    with open(output, encoding='utf-8') as fle:
+    assert "Wrote file to: " in result.stdout()
+    test_file = test_dir.joinpath("gatorgrader.yml")
+    with mocker.patch('builtins.input', return_value=""):
+        assert test_file.exists()
+    with open(test_file, encoding='utf-8') as fle:
+        #print(fle.read())
         assert "name: Project" in fle.read()
 
 
