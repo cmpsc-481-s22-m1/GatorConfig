1dfc5661a081b30271b783fc86d2b35a283ecae1
colemank <colemank@allegheny.edu>
2022-01-31 17:06:50 -0500

    Change format of default checks

--- gatorconfig/gator_config.py
+++ gatorconfig/gator_config.py
@@ -78,7 +78,9 @@ def get_checks(file: List[Path]) -> Dict:
     for item in file:
         running = True
         print("")
-        check_list = ['--exists', 'fragment "TODO" --count 0']
+        check_list = ['--description "Confirm the file exists" ConfirmFileExists',
+                      '--description "Make sure there are no TODOs in the file"'
+                      ' MatchFileFragment --fragment "TODO" --count 0']
         while running:
             check = input(f"Enter a check for {item} (Press \"Enter\" to move on): ")
             if check.lower() == "":
